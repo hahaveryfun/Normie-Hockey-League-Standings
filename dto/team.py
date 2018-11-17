b1ddb3eb79b1c31ded654db1935fbf964fa4c1c5
William Dunkerley <william@clio.local>
2018-11-17 18:22:35 -0500

    Non functional code, please ignore

---
+++ dto/team.py
@@ -0,0 +1,13 @@
+class Team:
+	def __init__(self, , _year):
+		if isinstance(_teams, list) and isinstance(_year, str):
+			self.teams = _teams
+			self.year = year
+		else:
+			raise Exception("_teams must be of type list and _year must be of type string")
+
+	def get_teams(self):
+		return self.teams
+
+	def get_year(self):
+		return self.year
